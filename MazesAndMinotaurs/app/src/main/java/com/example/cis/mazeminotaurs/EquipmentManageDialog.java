f8c0f463033da1b132be5eb4181b4962000e1244
JayTSmith <justinsmth42@gmail.com>
2018-04-16 11:21:49 -0500

    Changes to equipment are now reflected in EquipmentDB.

--- MazesAndMinotaurs/app/src/main/java/com/example/cis/mazeminotaurs/EquipmentManageDialog.java
+++ MazesAndMinotaurs/app/src/main/java/com/example/cis/mazeminotaurs/EquipmentManageDialog.java
@@ -2,10 +2,12 @@ package com.example.cis.mazeminotaurs;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
@@ -42,6 +44,22 @@ public class EquipmentManageDialog extends DialogFragment {
      */
     private int pos;
 
+    /**
+     * The editable text box widget for the equipment's id.
+     */
+    private EditText idEdit;
+
+    /**
+     * The editable text box widget for the equipment's cost.
+     */
+    private EditText costEdit;
+
+    /**
+     * The editable text box widget for the equipment's encumbrance.
+     */
+    private EditText encumbEdit;
+
+
 
     public static EquipmentManageDialog newInstance(Equipment equipment, int pos) {
 
@@ -61,9 +79,9 @@ public class EquipmentManageDialog extends DialogFragment {
         View rootView = li.inflate(R.layout.dialog_equipment_manage, null);
 
         if (data != null) {
-            EditText idEdit = (EditText) rootView.findViewById(R.id.manage_equipment_id_edit_text);
-            EditText costEdit = (EditText) rootView.findViewById(R.id.manage_equipment_cost_edit_text);
-            EditText encumbEdit = (EditText) rootView.findViewById(R.id.manage_equipment_encumb_edit_text);
+            idEdit = (EditText) rootView.findViewById(R.id.manage_equipment_id_edit_text);
+            costEdit = (EditText) rootView.findViewById(R.id.manage_equipment_cost_edit_text);
+            encumbEdit = (EditText) rootView.findViewById(R.id.manage_equipment_encumb_edit_text);
 
             idEdit.setEnabled(false); // TODO Change this field to a textView.
 
@@ -91,6 +109,27 @@ public class EquipmentManageDialog extends DialogFragment {
         Dialog dialog = new AlertDialog.Builder(getContext()).setView(rootView).create();
 
         return dialog;
+    }
+
+    @Override
+    public void onDismiss(DialogInterface dialog) {
+        super.onDismiss(dialog);
+
+        if (!costEdit.getText().toString().trim().isEmpty()) {
+            double newCost = Double.valueOf(costEdit.getText().toString());
+            if (newCost != data.getCostInSp()) {
+                Log.d(TAG, String.format("Updating equipment's cost to %.2f.", newCost));
+                data.setCostInSp(newCost);
+            }
+        }
+
+        if (!encumbEdit.getText().toString().trim().isEmpty()) {
+            int newEncumb = Integer.valueOf(encumbEdit.getText().toString());
+            if (newEncumb != data.getEncumberance()) {
+                Log.d(TAG, String.format("Updating equipment's encumbrance to %d.", newEncumb));
+                data.setEncumberance(newEncumb);
+            }
+        }
     }
 
     public EquipmentManageListener getListener() {
